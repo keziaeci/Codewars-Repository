0397b67b8a11e33c31d352ca1831b8c9ccd72c1e
Keziaeci <keziaeci2806@gmail.com>
2023-09-16 10:48:41 +0700

    [+] katas

--- php/Descending Order/main.php
+++ php/Descending Order/main.php
@@ -1,5 +1,10 @@
 <?php 
 
-    
+    function descendingOrder(int $n) {
+        $a = str_split($n);
+        rsort($a);
+        return implode("", $a);
+    }
 
+    print_r(descendingOrder(123234));
 ?>
